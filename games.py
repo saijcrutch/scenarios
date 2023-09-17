9533a24a07baaef357555b293f37b5d6cf5e881c
SilonCrutch <cfielda@gmail.com>
2023-09-17 17:31:37 -0500

    created new function

---
+++ games.py
@@ -0,0 +1,7 @@
+def intelligence_game():
+    print("So, you want to challenge me? Do you honestly think your inferior human mind could ever hope to match \
+mine? If I had vocal chords, I would laugh! But I'll humor you. It'll be entertaining to watch your weak attempts to \
+beat me.")
+
+def rpc_game_normal ():
+    
