c45e2b2ab1ecff3400a5cc2a173c9bc57cc7d247
SilonCrutch <cfielda@gmail.com>
2023-09-18 16:26:49 -0500

    created new function

--- rpc.py
+++ rpc.py
@@ -5,19 +5,35 @@ def rpc_game_normal ():
     computer_wins = 0
     wins = 0
     
-
+    print(" ")
 
     while (wins-computer_wins < 2) and (computer_wins-wins < 2) and (computer_wins < 3) and (wins < 3):
         print(f"Me: {computer_wins}  | You: {wins} ")
         user = input('Your move human: ').lower()
         computer = random.choice(rpc)
 
+        if user == 'r':
+            user = 'rock'
+        elif user == 's':
+            user = 'scissors'
+        elif user == 'p':
+            user = 'paper'
+        else:
+            print("Your human tricks won't work with me. Choose a valid character.")
+
+        if computer == 'r':
+            computer = 'rock'
+        elif computer == 's':
+            computer = 'scissors'
+        elif computer == 'p':
+            computer = 'paper'
+
         if user == computer:
             wins += 0
             computer_wins += 0
         elif pattern(user, computer):
             computer_wins += 1
-        else:
+        elif user ==:
             wins += 1
         
         print(f"I chose {computer} and you chose {user}.")
@@ -29,6 +45,11 @@ def rpc_game_normal ():
 def pattern(user, computer):
     if (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') or \
         (user == 's' and computer == 'p'):
+        return True
+    
+def user_pattern(user, computer):
+    if (computer == 'r' and user == 's') or (computer == 'p' and user == 'r') or \
+        (computer == 's' and user == 'p'):
         return True
     
 def score_n(user, computer):
