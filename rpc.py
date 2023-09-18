15a910524c06c0e6d2f440de052827d595b42683
SilonCrutch <cfielda@gmail.com>
2023-09-18 17:31:07 -0500

    fixed a bug that was preventing code from running

--- rpc.py
+++ rpc.py
@@ -18,8 +18,6 @@ def rpc_game_normal ():
             user = 'scissors'
         elif user == 'p':
             user = 'paper'
-        else:
-            print("Your human tricks won't work with me. Choose a valid character.")
 
         if computer == 'r':
             computer = 'rock'
@@ -28,28 +26,32 @@ def rpc_game_normal ():
         elif computer == 'p':
             computer = 'paper'
 
+        print(f"You chose {user} and I chose {computer}.")
+
         if user == computer:
             wins += 0
             computer_wins += 0
         elif pattern(user, computer):
             computer_wins += 1
-        elif user ==:
+        elif user_pattern(user, computer):
             wins += 1
+        else:
+            print("Your human tricks won't work with me. Choose a valid character.")
         
-        print(f"I chose {computer} and you chose {user}.")
+        
         
         if computer_wins == 3 or wins == 3 or wins-computer_wins == 2 or computer_wins-wins == 2:
             print(f"Me: {computer_wins}  | You: {wins} ")
         score_n(wins, computer_wins)
         
 def pattern(user, computer):
-    if (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') or \
-        (user == 's' and computer == 'p'):
+    if (user == 'rock' and computer == 'scissors') or (user == 'paper' and computer == 'rock') or \
+        (user == 'scissors' and computer == 'paper'):
         return True
     
 def user_pattern(user, computer):
-    if (computer == 'r' and user == 's') or (computer == 'p' and user == 'r') or \
-        (computer == 's' and user == 'p'):
+    if (computer == 'rock' and user == 'scissors') or (computer == 'paper' and user == 'rock') or \
+        (computer == 'scissors' and user == 'paper'):
         return True
     
 def score_n(user, computer):
