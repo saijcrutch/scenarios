81aa7daa2fb77c6ddd87c962db64c7aab58bd9af
SilonCrutch <cfielda@gmail.com>
2023-10-04 19:14:03 -0500

    made change

--- intelligence.py
+++ intelligence.py
@@ -5,9 +5,10 @@ def intelligence_game():
     from rpc import rpc_game_normal
     from hangman import hangman_n
     from number_game import number_n
+    from auxiliary_functions import computerSpeak
 
     print("So, you want to challenge me? Do you honestly think your inferior human mind could ever hope to match \
 mine? If I had vocal chords, I would laugh! But I'll humor you. It'll be entertaining to watch your weak attempts to \
 beat me.")
-
+    
 
