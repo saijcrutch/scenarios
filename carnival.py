8cb6e0d1c973c973930cc15305e3fb1ae219361c
saijcrutch <124953725+saijcrutch@users.noreply.github.com>
2023-10-06 16:16:47 -0500

    added new string

--- carnival.py
+++ carnival.py
@@ -11,3 +11,6 @@ Plus, we're the only carnival in town so you don't have much of a choice.")
     carnivalTalk("In this game, you have to try and remember the sequence of letters and numbers \
 before me. Let's test how good your memory is!")
     typeAttack()
+    carnivalTalk("Here's a really special game that I'm sure you'll love. You have to guess the \
+number of the box and claim your prize. Who knows, you may win big!")
+    mystery()
