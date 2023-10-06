837642d2f8e7891e0dd4f83aea7a94302c41dbdf
SilonCrutch <cfielda@gmail.com>
2023-10-05 20:34:39 -0500

    corrected some errors

--- hangman.py
+++ hangman.py
@@ -24,28 +24,28 @@ def hangman_n():
             if user in word_letters:
                 word_letters.remove(user)
         elif user in used_letters:
-            computerSpeak("Your human memory must be failing you. You already used that letter.")
+            computerSpeak("Your human memory must be failing you. You already used that letter.", 0.05)
         elif user == '1':
             user = input("Oh so you think you know it? I'd like to see you try! ").upper()
             if user == word:
-                computerSpeak(f"I'm speechless! I can't believe you guessed {word}. I guess you are kind of smart.")
+                computerSpeak(f"I'm speechless! I can't believe you guessed {word}. I guess you are kind of smart.", 0.05)
                 main.points += 2
                 break
             else:
-                computerSpeak(f"I knew you couldnt guess {word}! Guess is was too much for your memory.")
+                computerSpeak(f"I knew you couldnt guess {word}! Guess is was too much for your memory.", 0.05)
                 lives -= lives
                 break
-        elif user not in word and user not in used_letters:
-            print(computerSpeak("Wrong. Try again."))
+        elif user not in word_letters and user not in used_letters:
+            computerSpeak("Wrong. Try again.", 0.05)
             lives -= 1
 
-
         if len(word_letters) == 0 and lives > 0:
-            computerSpeak(f"You guessed {word}, surprisingly.")
+            computerSpeak(f"You guessed {word}, surprisingly.", 0.05)
             main.points += 1
 
 
 def valid_words(x):
+    from words import words
     # for normal and easy
     word = random.choice(words)
     while '-' in word or ' ' in word:
--- intelligence.py
+++ intelligence.py
@@ -8,13 +8,18 @@ def intelligence_game():
     from auxiliary_functions import computerSpeak
 
     computerSpeak("So, you want to challenge me? Do you honestly think your inferior human mind could ever hope to match \
-mine? If I had vocal chords, I would laugh! But I'll humor you. It'll be entertaining to watch your weak attempts to \
-beat me.", 0.05)
+mine? If I had vocal chords, I would laugh! But I'll humor you. It'll be entertaining to watch your weak attempts at \
+beating me.", 0.05)
     computerSpeak("The first challenge is a game of hangman. I'll be surprised if you can manage to guess the word I'm thinking \
 of.", 0.05)
     hangman_n()
+    print("---")
     computerSpeak("Try not to hurt yourself guessing my number.", 0.05)
     number_n()
+    print("---")
     computerSpeak("You for sure can't beat me in a game of rock, paper, scissors.", 0.05)
     rpc_game_normal()
+    print("---")
     computerSpeak("No matter what, I will always be better than you.", 0.05)
+
+intelligence_game()
